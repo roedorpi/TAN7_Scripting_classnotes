fc4dc8655efd95e42ac76302bd2bbbfecc68981e
roedorpi <rop@es.aau.dk>
2023-09-27 14:57:02 +0200

    material for session 03 example with the system usability scale

--- SUS_evaluation.py
+++ SUS_evaluation.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 sus_scale = {
  1 : "I think that I would like to use this system frequently",
  2 : "I found the system unnecessarily complex",
@@ -9,3 +11,16 @@ sus_scale = {
  8 : "I found the system very cumbersome to use",
  9 : "I felt very confident using the system",
  10 : "I needed to learn a lot of things before I could get going with this system"}
+
+print("System usability scale.")
+user_data = {}
+while True:
+   name = input('What is your name? ')
+   user_data[name] = {}
+   for k, q in sus_scale.items():
+       answer = int(input(q))
+       user_data[name][k] = [q, answer]
+   if input('More subjects (yes/no)?').strip().lower() == 'no':
+       break
+print("Thanks for participating!")
+print(user_data)
