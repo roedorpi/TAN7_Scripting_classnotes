087e784929e261481993f6e517ea46ae44190bee
roedorpi <rop@es.aau.dk>
2023-10-03 09:56:59 +0200

    Exercise solution for session03

--- session03_exercises.py
+++ session03_exercises.py
@@ -48,3 +48,171 @@ pancakes: 1 1/2 cup flour, 3 1/2 teaspoons baking powder, 1/4 teaspoon salt,
 muffins:  2 cups flour, 3 teaspoons baking powder, 1/2 teaspoon salt, 
           3/4 cup sugar, 1 egg, 1 cup milk, 1/4 cup oil
 """
+
+from collections import defaultdict
+
+ingredients = dict()
+
+# every value of this dict is a list of items,
+# each item is in its self a list with
+# format: [<amount>, <unit>, <ingredient>]
+
+ingredients["brownies"] = [[1.5, "cup", "sugar"],
+                           [0.75, "cup", "flour"],
+                           [0.66, "cup", "cocoa powder"],
+                           [0.5, "cup", "chocolate chips"],
+                           [2, "piece", "egg"],
+                           [0.5, "cup", "oil"],
+                           [2, "tablespoon", "water"],
+                           [0.5, "teaspoon", "vanilla"]]
+
+ingredients["pancakes"] = [[1.5, "cup", "flour"],
+                           [3.5, "teaspoon", "baking powder"],
+                           [0.25, "teaspoon", "salt"],
+                           [1, "cup", "milk"],
+                           [1, "tablespoon", "sugar"],
+                           [1, "piece", "egg"]]
+
+ingredients["muffins"] = [[2, "cup", "flour"],
+                          [3, "teaspoon", "baking powder"],
+                          [0.5, "teaspoon", "salt"],
+                          [0.75, "cup", "sugar"],
+                          [1, "cup", "milk"],
+                          [0.25, "cup", "oil"],
+                          [1, "piece", "egg"]]
+
+# ### operations:
+# list available recipes:
+
+print("These recipes are available:\n")
+for availableRecipes in ingredients:
+    print(availableRecipes)
+print("\n")
+
+# user input: what would you like to bake?
+
+wantedName = input("What would you like to bake? ")
+
+# to bake one portion of wanted recipe:
+
+print("To bake one portion of", wantedName, "you will need:\n")
+for eachIngredient in ingredients[wantedName]:
+    print(eachIngredient[0], eachIngredient[1] + "s", eachIngredient[2])
+
+# user input: how many would you like to bake?
+
+wantedAmount = float(input("How many portions? "))
+
+# to make X amount you need the following shopping list:
+
+print("To bake", wantedAmount, "portions of", wantedName, "you need:\n")
+for neededIngredient in ingredients[wantedName]:
+    neededAmount = neededIngredient[0] * wantedAmount
+    print(neededAmount, neededIngredient[1] + "s of", neededIngredient[2])
+
+# to make one portion of each recipe in cookbook:
+
+ultimateAmount = defaultdict(float)
+ultimateUnit = dict()
+
+for availableRecipes in ingredients:
+    for eachIngredient in ingredients[availableRecipes]:
+        ultimateAmount[eachIngredient[2]] += eachIngredient[0]
+        ultimateUnit[eachIngredient[2]] = eachIngredient[1]
+
+print("To bake one portion of all available recipes you need to buy:\n")
+for eachIngredient in ultimateAmount:
+    print(ultimateAmount[eachIngredient], ultimateUnit[eachIngredient] + "s of", eachIngredient)
+
+# ## part b code
+
+# how much of each ingredient do you have at home? user input
+
+userAmount = dict()
+skipRedundancy = set()
+
+for availableRecipes in ingredients:
+    for eachIngredient in ingredients[availableRecipes]:
+        if eachIngredient[2] in skipRedundancy:
+            pass
+        else:
+            userAmount[eachIngredient[2]] = float(
+                input(f"how many {eachIngredient[1]}s of {eachIngredient[2]} do you have? "))
+            skipRedundancy.add(eachIngredient[2])
+
+print(userAmount)
+
+# do you have enough ingredients in your kitchen to bake one portion of recipe of choice (user input)?
+
+recipeCheck = input("I would like to bake one portion of: ")
+missingIngredient = list()
+
+for eachIngredient in ingredients[recipeCheck]:
+    if eachIngredient[0] > userAmount[eachIngredient[2]]:
+        missingIngredient.append(eachIngredient[2])
+
+if len(missingIngredient) == 0:
+    print(f"CONGRATULATIONS! You can bake one portion of {recipeCheck}!")
+else:
+    print(f"Sorry, you do not have enough of: {missingIngredient}.")
+
+# which recipes could you make one portion of, given the ingredients you have in your kitchen?
+
+canMake = set()
+
+for availableRecipes, recipeIngredients in ingredients.items():
+    for eachIngredient in recipeIngredients:
+        if eachIngredient[0] <= userAmount[eachIngredient[2]]:
+            canMake.add(availableRecipes)
+
+print(f"You have enough ingredients to make one portion of the following recipes: {canMake}")
+
+# if you want to make X portions (user input), which recipes could you make X portions of?
+
+canMakeEnough = set()
+howManyWant = int(input("How many guests will you host? "))
+
+for availableRecipes, recipeIngredients in ingredients.items():
+    for eachIngredient in recipeIngredients:
+        if eachIngredient[0] * howManyWant < userAmount[eachIngredient[2]]:
+            canMakeEnough.add(availableRecipes)
+
+if len(canMakeEnough) == 0:
+    print(f"you cannot make any recipes if {howManyWant} guests come over.")
+else:
+    print(f"You have enough ingredients to make {canMakeEnough} for {howManyWant} guests!")
+
+# ## part c code
+
+# user adding recipes to cookbook
+
+newRecipeName = input("what is this recipe for? ")
+newRecipe = list()
+
+howManyIngredients = int(input("how many ingredients? "))
+newIngredientCount = 0
+
+print("I will ask you to write one ingredient at a time: what it is, then what unit it is measured in, \
+then how much of that unit is needed in recipe. For example, 1 cup flour means you first write \
+flour, then cup, then 1.")
+
+while newIngredientCount < howManyIngredients:
+    whatIngredient = input(f"enter ingredient {newIngredientCount + 1}: ")
+    whatMeasureIng = input("how is it measured? (options: cup, tablespoon, teaspoon, piece): ")
+    howMuchIng = float(input("how much is needed? "))
+
+    newIngredient = list()
+    newIngredient.append(howMuchIng)
+    newIngredient.append(whatMeasureIng)
+    newIngredient.append(whatIngredient)
+
+    newRecipe.append(newIngredient)
+
+    newIngredientCount += 1
+
+ingredients[newRecipeName] = newRecipe
+
+print("The following recipes are available to you: ")
+for availableRecipes in ingredients:
+    print(availableRecipes)
+
