bf91c5f11598d1407952aecd7de2fcb8d473a73b
roedorpi <rop@es.aau.dk>
2023-10-05 10:52:40 +0200

    exercise description for session04

---
+++ session04_exercises.py
@@ -0,0 +1,17 @@
+"""
+Exercises session 04.
+
+Make a program that can read questions from a text file, present these questions to a user and save responses into a csv file.
+
+The program has to:
+    1. run until it receives the instructions from the user to stop.
+    2. avoid crashing if the question or response file are not found or can't be opened
+    3. register and answer and response time for each answer
+    4. after the questionnaire is compleat it should print the average score (or calculate a result) for the individual as well as the average for all respondents in the answer file.
+
+"""
+# Hint: Start with the SUS_evaluation.py script for inspiration.
+# Hint: use try -- execpt conditionals to avoid program crashes in file operations.
+# Hint: open question file in read mode and csv file in append mode.
+# Hint: for response time look at the time and datetime libraries.
+
