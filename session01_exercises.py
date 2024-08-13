5fd01ad748b85f0c8f87a7304f9b91fe4371094c
rop <roedorpi@gmail.com>
2024-08-13 15:39:33 +0200

    Preparing for e24, removed solutions.

---
+++ session01_exercises.py
@@ -0,0 +1,19 @@
+"""
+Session 01 exercise solutions
+Exercise 1
+Make a program that takes a string and an integer as input from the user
+and returns a new string containing the letters from the original string
+shifted in the alphabet by the given amount.
+For example, “cheer” shifted by 7 is “jolly” and “melon” rotated by −10
+is “cubed”.
+"""
+# this is an external library that gives the functionality to terminate the program
+import sys
+# Make strings that contain all the letters of the alphabet in the correct order.
+
+
+"""
+Make a program that changes temperature in degrees Celsius to Fahrenheit, 
+using the conversion: C*9/5 + 32 = F
+
+"""
