f7c751eb88ac1299d3da461c0b0d427faa6e7213
roedorpi <83291185+roedorpi@users.noreply.github.com>
2024-10-04 08:32:28 +0200

    solutions of session02 and updated on session02 files

--- session02_lists.py
+++ session02_lists.py
@@ -3,19 +3,21 @@ LISTS
 definition: a list is a collection of items in a particular order (position matters in lists)
 you can put anything into a list
 """
-
+##
 vegetables = ['potato', 'carrot', 'beetroot', 'onion']
 print(vegetables)
 
+##
 """
 different ways to create lists:
 """
-
 L1 = list()  # this creates an EMPTY list (one with 0 elements)(L1 is a variable of A LIST)
 L2 = []  # this is also an EMPTY list
 L3 = [1, 2, 3]  # this is a non-empty list with elements separated by commas ,
 L4 = [1, "two", 3.1415]  # you can mix types in lists!
 L5 = [1, "two", [10, "eleven", 12], []]  # you can put lists in lists too!
+
+##
 """
 finding item in list by position
 item order from beginning: 0, 1, 2, 3...
@@ -28,7 +30,6 @@ for a four-item list, the positions are:
  A   B   C   D
 -4  -3  -2  -1
 """
-
 print(vegetables[0])
 print(vegetables[0].title())
 print(vegetables[1])
@@ -37,31 +38,26 @@ print(vegetables[-2])
 
 vegetables[2] == vegetables[-2]
 
-
+##
 """
 L[0] is the first element
-
 len(L) gives you the number of elements
-
 L[-len(L)] is also the first element
-
 L[3:5] is the sublist or slice containing the 4th and 5th element of the list
 """
-
 message = f"Please remember to buy one {vegetables[0]}."
 print(message)
 
 otherMessage = f"{vegetables[0].title()} is my favorite vegetable!"
 print(otherMessage)
 
+##
 """
 Using an integer variable as index:
 """
-
 a = 2
-vegetables[a]
-
-
+print(vegetables[a])
+##
 """
 Nested lists 
 """
@@ -73,8 +69,9 @@ funlist = [1, [2, [3, [4], 5], 6], 7]
 print(funlist[1])
 print(funlist[1][0]) # print the zero element of element 1
 print(funlist[1][1][1][0]) # element 4
-funlist[1][1][1] # list containing element 4
+print(funlist[1][1][1]) # list containing element 4
 
+##
 """
 A nightmare list, here you have an example of a nested list with elements of different types. 
 """
@@ -86,11 +83,12 @@ A nightmare list, here you have an example of a nested list with elements of dif
 headachelist = [1, 2, ["earth", [3.0, 4.0, ["fire", "water"], ["air"], True, False, [], ]]]
 
 # address the i in fire
-headachelist[2][1][2][0][1]
+print(headachelist[2][1][2][0][1])
 
 # address the i in air
-headachelist[2][1][3][0][1]
+print(headachelist[2][1][3][0][1])
 
+##
 """
 Changing, adding, and removing elements
 
@@ -100,7 +98,7 @@ modifying elements
 vegetables[0] = 'peas'
 print(vegetables)
 
-
+##
 """
 Adding elements using .append() - append adds at the end so you can build lists dynamically. 
 This is useful if you need to generate data while the program is running. 
@@ -111,7 +109,7 @@ print(vegetables)
 
 vegetables.append('peas')
 print(vegetables)
-
+##
 vegetables = [] # empty list we will fill out
 vegetables.append('potato')
 vegetables.append('carrot')
@@ -119,14 +117,13 @@ vegetables.append('beetroot')
 vegetables.append('peas')
 
 print(vegetables)
-
+##
 """
 inserting elements into a specific position in the list
-
 """
 vegetables.insert(1, 'peas')
 print(vegetables)
-
+##
 """
 removing elements from the list when you know the exact position of the item using the del keyword.
 """
@@ -136,6 +133,7 @@ print(vegetables)
 
 del vegetables[2]
 print(vegetables)
+##
 """
 Removing an item using .pop() - takes off the last item from the list and stores it as a new variable
 """
@@ -153,6 +151,7 @@ print(vegetables)
 
 loved_vegetables = vegetables.pop(0)
 print(f"The vegetable I love the most is {loved_vegetables}.")
+##
 """
 Removing an item by value
 remember that .remove() only removes the first occurrence in a list, so if an item shows up multiple times, 
@@ -183,6 +182,7 @@ for fruit in fruits:
 
 print(fruits)
 
+##
 """
 Tuples
 an immutable list is called a tuple
@@ -200,7 +200,7 @@ print(vegetables)
 
 vegetables[1] = 'peas'
 print(vegetables)
-
+##
 """
 Organizing lists
 sorting permanently with .sort() - alphabetic sorting
@@ -230,6 +230,8 @@ print(fruits)
 
 print("\nHere is the reverse-sorted list:")
 print(sorted(fruits, reverse=True))
+##
+
 """
 Reverse order of the list permanently using reverse(). This is not a reverse alphabetically as in the example 
 with sort(), were only the order of the elements is reversed. 
@@ -243,10 +245,10 @@ print(fruits)
 fruits.reverse()
 print(fruits)
 
+##
 """
 find length of a list
 """
-
 len(fruits)
 """
 Avoiding index errors when working with lists
@@ -258,7 +260,7 @@ Finding position of a specific item
 """
 fruit.index('banana')
 
-
+##
 """
 Indexing in lists a little exercise. Using the following list, write the index notation 
 to retrieve the desired output. 
@@ -267,7 +269,7 @@ individual elements.
 In the console write:
 >>> omgWhy[2]
 >>> omgWhy[2][3] 
-and so on... look at what the interpreter write back and try to get to the correct answer. 
+and so on... look at what the interpreter writes back and try to get to the correct answer. 
 
 """
 
---
+++ session02_solutions.py
@@ -0,0 +1,198 @@
+"""
+Problem 1.
+write a chatbot program:
+
+1) the program asks the user for their name, and then tells them it is a very pretty name
+2) the program asks the user if they have any fun weekend plans, and then tells them it sounds like a lot of fun
+3) the program asks the user what is their favorite color, and then responds that (the favorite color) is also the
+   chatbot's favorite color
+4) the program asks the user to write a math equation, and then provides the answer to that equation
+5) the program thanks the user for the conversation and wishes them a nice day
+
+"""
+# Hints:
+
+# assign variable names to input, and use those variables in fstrings in print input function always returns a string
+# for the math question, you can only add integers (cannot add strings to each other)
+##
+userName = input("Hello, what is your name? ")
+
+# print a compliment about their name
+print(f"{userName.title()} is such a lovely name!")
+
+# ask the user to input their weekend plans
+weekendPlans = input(f"So {userName.title()}, what are your plans for the weekend? ")
+
+# print a message about the weekend plans being fun, regardless of their input
+print("That sounds like so much fun!")
+
+# ask the user about their favorite color
+favColor = input("What is your favorite color? ")
+
+# print a message complimenting that specific color
+print(f"{favColor.title()} is my favorite too.")
+
+# print a message explaining that you will ask the user for two numbers
+print("Let's play a game. Give me two numbers and I will add them!")
+
+# ask the user to input the first number, and convert that number to integer right away
+equation1 = int(input("first number: "))
+
+# ask the user to input the second number, and convert that number to integer right away
+equation2 = int(input("second number: "))
+
+# calculate sum of two numbers
+sumBoth = equation1 + equation2
+
+
+# print the answer
+print(f"The sum of {equation1} and {equation2} is {sumBoth}!")
+
+# use the function eval() to evaluate a mathematical expression.
+# look up the use of the function on the python documentation
+eq_literal = input('Write a math equation that Python can understand: ')
+eq_result = eval(eq_literal)
+
+print(f"The result is: {eq_result}.")
+
+# print a farewell message
+print("Thank you for talking to me, and have a nice day!")
+
+
+"""
+Problem 2.
+Collecting user feedback (and manipulating user reviews)
+Write code so users can rate their experience using a product.
+
+The user needs to provide the following information: user name, product name, star rating, any other comments
+BUT! the user should not able to give a rating lower than 4 or 5 stars; if the user inputs a rating of 3 or lower, your 
+program will keep asking the user if they're sure, repeatedly, until the user inputs 4 or 5.
+"""
+# Hints
+# Since this stops when a condition is met, we will be using a while loop, so start with setting up a flag
+
+# Save data into a list of lists, with the information provided by the user
+
+# inside the previous while loop, write another while loop to decide what happens when the user gives the undesirable
+# rating to your product
+
+# ask the user if they want to input another review, and if not - switch the flag from off.
+##
+print("Welcome to the product rating program.")
+user_data = []
+collecting_data = True
+while collecting_data:
+    name = input('What is your name? ')
+    product = input("what is the product? ")
+    rating = int(input("what is your rating, out of 5? "))
+    while rating <= 3:
+        rating = int(input("are you sure? try rating again, out of 5: "))
+    comments = input("any other comments? ")
+    user_data.append([name, product, rating, f"{comments}"])
+    repeat = input("Would you like to add another person? (y/n) ")
+    if repeat == 'n':
+        collecting_data = False
+
+print("Thanks for participating!")
+print(user_data)
+"""
+Problem 3. 
+count days between two user-defined dates
+write a script that calculates how many days are between two user-defined dates
+confirm that your results are correct here: https://www.timeanddate.com/date/duration.html
+
+PLEASE NOTE: if you google this challenge, you will find a very simple and elegant solution using datetime package. 
+Try writing this script WITHOUT using datetime, and using your own logic and the programming knowledge you have from 
+class. If you are having trouble putting this into code, try writing a flowchart of your algorithm
+
+There will NOT be one correct answer to this challenge; the hints below are hints to the code I wrote, 
+but there are more ways you can solve this problem.  
+ 
+"""
+# Hints:
+# define assumptions ##
+# a leap year occurs every 4 years except years 1700, 1800, 1900, and 2100
+# different months have different durations
+
+#program steps#
+# ask user to specify two dates
+# hint: when you are writing code, and troubleshooting it, you can define your own dates here
+# this means that you define two dates that you want to use as variables, and then write the rest of your code
+# once your code works, you come back to this cell and convert your variables into input() remember that input function
+# always returns a string! also remember that your user might want to put a later date as the first input, and an
+# earlier date as the second input you need to tell users exactly what to do, otherwise they will do silly things that
+# can mess up your code
+##
+leap_years = set(range(1700, 2500, 4)) - {1700, 1800, 1900, 2100}
+days_in_months = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+date1 = input('Enter a date in the format DD MM YYYY: ')
+date2 = input('Enter a second date in the format DD MM YYYY: ')
+
+# find out which date is earlier
+numdate1 = []
+numdate2 = []
+for i in date1.split(): numdate1.append(int(i))
+for i in date2.split(): numdate2.append(int(i))
+
+# compare years
+if numdate1[2] < numdate2[2]:
+    first_date = numdate1
+    second_date = numdate2
+elif numdate1[2] > numdate2[2]:
+    first_date = numdate2
+    second_date = numdate1
+else: # it is the same year lets compare the months
+    if numdate1[1] < numdate2[1]:
+        first_date = numdate1
+        second_date = numdate2
+    elif numdate1[1] > numdate2[1]:
+        first_date = numdate2
+        second_date = numdate1
+    else: # same month lets compare the days
+        if numdate1[0] < numdate2[0]:
+            first_date = numdate1
+            second_date = numdate2
+        elif numdate1[0] > numdate2[0]:
+            first_date = numdate2
+            second_date = numdate1
+        else:
+            print("You entered the same date twice!")
+            first_date, second_date = numdate1
+
+## calculate how many days from the first date to the end of that year.
+first_to_endofyear = 0
+
+#days to the end of the month
+first_to_endofyear += days_in_months[first_date[1]-1] - first_date[0]
+
+#full months to the end of the year
+for month in range(first_date[1], 12):
+    first_to_endofyear += days_in_months[month]
+
+if first_date[1] <= 2 and first_date[2] in leap_years:
+    first_to_endofyear += 1
+
+## calculate how many days from the second date to the beginning of that year.
+second_to_startofyear = 0
+
+# days to the start of the month
+second_to_startofyear += second_date[0]
+
+# full months to the start of the year
+for month in range(0, second_date[1]-1):
+    second_to_startofyear += days_in_months[month]
+
+if second_date[1] <= 2 and second_date[2] in leap_years:
+    second_to_startofyear += 1
+
+## How many days are in the full years in between the two dates
+years_btw = set(range(first_date[2]+1,second_date[2]))
+leap_years_btw = leap_years.intersection(years_btw)
+in_between = int(len(years_btw)*365) + int(len(leap_years_btw))
+
+## Calculate total sum
+if first_date[2] != second_date[2]:
+    how_many_days = first_to_endofyear + second_to_startofyear + in_between
+else:
+    how_many_days = second_to_startofyear + first_to_endofyear - 365
