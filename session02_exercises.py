145d3eb37d18c139c55535174e66241b15acca0f
roedorpi <83291185+roedorpi@users.noreply.github.com>
2026-09-17 15:49:45 +0200

    Updating material for session02 about data structures loops and corpora

--- session02_exercises.py
+++ session02_exercises.py
@@ -6,8 +6,12 @@
 - create a loop that iterates over the list, set and dictionary and prints each item
 - use a list comprehension to create a new list with every other item from the original list
 - create a list of long words (more than 5 characters) from a given text and print them
+"""
 
+"""
 2. Use a dictionary to store a corpus of texts, where the keys are the document names and the values are the text content.
-- use the corpus and what you have learned to print out some basic statistics about the texts, such as the number of words, number of unique words, and average word length, lexical complexity, etc.
+- Use the corpus and what you have learned to print out some basic statistics about the texts, such as the number of words, number of unique words, and average word length, lexical complexity, etc.
+- Create the frequency distribution of the words in the corpus and print out the most common words. Do the same for each document in the corpus and compare the results. Are there any evident differences in the vocabulary used in each document?
+- Create the frequency distribution of the words in the corpus that are larger than 5 characters and print out the most common words. Do the same for each document in the corpus and compare the results. Are there any evident differences in the vocabulary used in each document?
 
 """
